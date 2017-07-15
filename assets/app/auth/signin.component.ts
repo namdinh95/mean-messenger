a766b7dda16e1072c7bedfe81244e8a69df88f4c
Nam Dinh <nam.dinhviet@gmail.com>
2017-07-14 22:19:29 -0500

    setup and frontend done

---
+++ assets/app/auth/signin.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html'
+})
+export class SigninComponent {
+  myForm: FormGroup;
+
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+      ]),
+      password: new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.myForm);
+    this.myForm.reset();
+  }
+}
---
+++ assets/app/messages/message-input.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MessageService } from './message.service';
+import { Message } from './message.model';
+
+@Component({
+  selector: 'app-message-input',
+  templateUrl: './message-input.component.html'
+})
+export class MessageInputComponent {
+  constructor(private messageService: MessageService) {}
+  onSubmit(form: NgForm) {
+    console.log(form);
+    const message = new Message(form.value.content, 'Nam');
+    this.messageService.addMessage(message);
+    form.resetForm(); // Clear form after submit
+  }
+}
