7b4b00172a7214978c75a38912fe57116bc41d25
Nam Dinh <nam.dinhviet@gmail.com>
2017-08-15 20:41:38 -0500

    Login now works

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 var User = require('../models/user');
 
@@ -25,4 +26,32 @@ router.post('/', function (req, res, next) {
   });
 });
 
+router.post('/signin', (req, res, next) => {
+  User.findOne({email: req.body.email}, (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        title: 'An error occured',
+        error: err
+      });
+    }
+    if (!user) {
+      return res.status(401).json({
+        title: 'Login failed',
+        error: {message: 'Invalid login credentials'}
+      });
+    }
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      return res.status(401).json({
+        title: 'Login failed',
+        error: {message: 'Invalid llogin credentials'}
+      });
+    }
+    var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+    res.status(200).json({
+      message: 'Login successful',
+      token: token,
+      userID: user._id
+    });
+  });
+});
 module.exports = router;
